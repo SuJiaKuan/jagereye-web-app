1b400fc82e0135dc18cfafcda708dd920630e4b7
SuJiaKuan <jiakuan.su@toppano.in>
2018-01-26 11:52:28 +0800

    Remove unnecessary routes

--- shared/routes.jsx
+++ shared/routes.jsx
@@ -7,11 +7,6 @@ import MainLayout from './containers/layouts/MainLayout.jsx';
 
 import DashboardPageContainer         from './containers/pages/DashboardPage.jsx';
 import CamerasPageContainer           from './containers/pages/CamerasPage.jsx';
-import ActivationsPageContainer       from './containers/pages/ActivationsPage.jsx';
-import ActivationPageContainer        from './containers/pages/ActivationPage.jsx';
-import ShareResultPageContainer       from './containers/pages/ShareResultPage.jsx';
-import CustomShareResultPageContainer from './containers/pages/CustomShareResultPage.jsx';
-import PromoPageContainer             from './containers/pages/PromoPage.jsx';
 
 export default (
     <Route component={App} >
@@ -19,22 +14,8 @@ export default (
         <Redirect from='' to='/dashboard' />
 
         <Route component={MainLayout} path='/'>
-            <Redirect from='/kmda/start' to='/activations?search=english' />
-
             <Route component={DashboardPageContainer} path='/dashboard' />
             <Route component={CamerasPageContainer} path='/cameras' />
-
-            <Route component={ActivationsPageContainer} path='/activations' />
-
-            <Route component={ActivationPageContainer} path='/activations/:id' />
-            <Route component={ActivationPageContainer} path='/activations/:id/:title' />
-
-
-            <Route component={ShareResultPageContainer} path='/result/:id/:accountId' />
-            <Route component={CustomShareResultPageContainer} path='/share/:key' />
-
-            <Route component={PromoPageContainer} path='/promo/:key' />
-
         </Route>
     </Route>
 );
